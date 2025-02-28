589cbbd53ea3588917bf8cab58c9966a3d337284
Pablo Ariel Cerbino <pcerbino@gmail.com>
2025-02-28 16:16:40 -0300

    Add agencies migration

---
+++ database/migrations/2025_02_28_191525_create_agencies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agencies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 64);
+            $table->string('email')->unique();
+            $table->text('secret');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agencies');
+    }
+};
