ef4d8750ded4b7a7a0888cdd2bd91d40ffcffd68
Pablo Ariel Cerbino <pcerbino@gmail.com>
2025-03-03 11:46:39 -0300

    Add Agency POST route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgencyController;
 
+Route::post('/agencies', [AgencyController::class, 'store']);
