b3ca2efce27ed61c48bd54c318f39be5753a4e58
Pablo Ariel Cerbino <pcerbino@gmail.com>
2025-03-03 12:11:21 -0300

    Add Agency GET route

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AgencyController;
 
 Route::post('/agencies', [AgencyController::class, 'store']);
+Route::get('/agencies', [AgencyController::class, 'show']);
